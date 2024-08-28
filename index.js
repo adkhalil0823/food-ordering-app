0ea20e26cc9647e62779d4d3e70cacb483b2e480
adkhalil0823 <adkhalil0823@gmail.com>
2024-08-28 09:36:24 +0300

    connect ejs template engine to the app

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
 app.listen(3000, () => {
   console.log("app listening on port 3000");
