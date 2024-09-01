e8c1247fb78c9ddf81125ce012607cbfa9eb2f5a
adkhalil0823 <adkhalil0823@gmail.com>
2024-09-01 08:38:41 +0300

    switch to a better handlebars engine

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const path = require("path");
-const { engine } = require('express-handlebars');
-const Handlebars = require("handlebars")
 const app = express();
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+var hbs = require('express-hbs');
 
 app.use(express.static("public"));
 
-Handlebars.registerHelper('loud', function (aString) {
-  return aString.toUpperCase()
-})
+// Use `.hbs` for extensions and find partials in `views/partials`.
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    firstname: "Yehuda",
-    lastname: "Katz",
-  });
+  res.render("index");
 });
 
 app.listen(3000, () => {
