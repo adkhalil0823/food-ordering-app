58b1c37ce77f82a2c4abb8d70ef616c11cd13dee
adkhalil0823 <adkhalil0823@gmail.com>
2024-09-01 03:12:57 +0300

    use a shorter express static path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
