86af44eb7d3e95929dc58c1bf8d17f67a6a41b45
adkhalil0823 <adkhalil0823@gmail.com>
2024-09-01 05:01:43 +0300

    switch from ejs to handlebars

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const path = require("path");
-
+const { engine } = require('express-handlebars');
+const Handlebars = require("handlebars")
 const app = express();
-app.set("view engine", "ejs");
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.use(express.static("public"));
 
+Handlebars.registerHelper('loud', function (aString) {
+  return aString.toUpperCase()
+})
+
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  res.render("index", {
+    firstname: "Yehuda",
+    lastname: "Katz",
+  });
 });
 
 app.listen(3000, () => {
