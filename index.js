7c49513f009c802104a6150ba2ef69b98710744d
adkhalil0823 <adkhalil0823@gmail.com>
2024-09-01 10:26:41 +0300

    switch to a newer and better handlebars engine

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const { engine } = require('express-handlebars')
+
 const app = express();
-var hbs = require('express-hbs');
+
 
 app.use(express.static("public"));
-
-// Use `.hbs` for extensions and find partials in `views/partials`.
-app.engine('hbs', hbs.express4({
-  partialsDir: __dirname + '/views/partials'
-}));
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get("/", (req, res) => {
   res.render("index");
